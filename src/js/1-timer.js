0a6f77b7fc02bd40635fba49edc8eecd4ecbebe1
lassandra85 <lassandra85@gmail.com>
2025-06-20 21:40:58 +0200

    hw

---
+++ src/js/1-timer.js
@@ -0,0 +1,105 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  input: document.querySelector('#datetime-picker'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+let userSelectedDate = null;
+let timerId = null;
+
+refs.startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const selectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (selectedDate <= currentDate) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+      refs.startBtn.disabled = true;
+    } else {
+      userSelectedDate = selectedDate;
+      refs.startBtn.disabled = false;
+    }
+  },
+};
+
+flatpickr(refs.input, options);
+
+refs.startBtn.addEventListener('click', () => {
+  if (!userSelectedDate) return;
+
+  refs.startBtn.disabled = true;
+  refs.input.disabled = true;
+
+  timerId = setInterval(() => {
+    const now = new Date();
+    const timeDiff = userSelectedDate - now;
+
+    if (timeDiff <= 0) {
+      clearInterval(timerId);
+      updateTimerDisplay(0);
+      refs.input.disabled = false;
+      return;
+    }
+
+    updateTimerDisplay(timeDiff);
+  }, 1000);
+});
+
+function updateTimerDisplay(ms) {
+  const { days, hours, minutes, seconds } = convertMs(ms);
+
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+//Напиши функцію, наприклад addLeadingZero(value),
+// яка використовує метод рядка padStart()
+// і перед відмальовуванням інтерфейсу форматує значення.
